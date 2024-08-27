9480e8678f97fc2d40f91c6cd04be4c6160cf663
Mask_X <xlyhacker@gmail.com>
2024-08-27 21:20:02 +0800

    login

---
+++ pages/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view class="login-page">
+		<view class="login-bk-container">
+			<image class="login-bk"></image>
+			
+			
+		</view>
+		<view class="login-actions">
+			<button class="login-btn">微信一键登录</button>
+			<view class="login-access">
+				<view class="agree-checkbox-box"></view>
+				<view class="access-text">我已阅读并同意<text>《用户协议》</text>和<text>《隐私政策》</text></view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
